1ce8c58de78e09e97e707e5fd2554a27a21ee6c1
Code <mac92j@pm.me>
2021-02-09 19:17:52 -0500

    Add transactions view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,7 @@
 class TransactionsController < ApplicationController
     def index
-        transactions = Transaction.all
-        render json: transactions
+        @transactions = Transaction.all
+        # render json: transactions
     end
     
     def show
