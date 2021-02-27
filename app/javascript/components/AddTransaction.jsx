0d333a4cb837062b9aa397519c7b753a66e3fa73
Code <mac92j@pm.me>
2021-02-27 15:05:42 -0500

    Add onChange function to AddTransaction component

--- app/javascript/components/AddTransaction.jsx
+++ app/javascript/components/AddTransaction.jsx
@@ -9,10 +9,15 @@ class AddTransaction extends React.Component {
       amount_out: ""
     };
     this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onSubmit(event) {
     event.preventDefault();
+  }
+
+  onChange(event) {
+    this.setState({[event.target.name]: event.target.value});
   }
 
   render() {
@@ -24,17 +29,17 @@ class AddTransaction extends React.Component {
           <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="payee_input">Payee</label>
-              <input type="text" name="payee" id="payee_input" className="form-control" />
+              <input type="text" name="payee" id="payee_input" className="form-control" onChange={this.onChange} />
             </div>
 
             <div className="form-group">
               <label htmlFor="description_input">Description</label>
-              <input type="text" name="description" id="description_input" className="form-control" />
+              <input type="text" name="description" id="description_input" className="form-control" onChange={this.onChange} />
             </div>
 
             <div className="form-group">
               <label htmlFor="amount_out_input">Amount Out</label>
-              <input type="text" name="amount_out" id="amount_out_input" className="form-control" />
+              <input type="text" name="amount_out" id="amount_out_input" className="form-control" onChange={this.onChange} />
             </div>
 
             <button type="submit" className="btn btn-primary">Add Transaction</button>
