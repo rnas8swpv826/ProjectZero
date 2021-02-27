71b26659fd20e5e0e249b414987f3e8ba608db98
Code <mac92j@pm.me>
2021-02-27 18:58:42 -0500

    Add create method to transactions controller

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,6 +1,23 @@
 class Api::TransactionsController < ApplicationController
-  def index
+  def index # index is a method
     transactions = Transaction.all
     render json: transactions
+  end
+
+  def create
+    transaction = Transaction.new(transaction_params)
+    if transaction.save
+      render json: transaction
+    else
+      render json: {
+        data: transaction.errors
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:payee, :description, :amount_out)
   end
 end
