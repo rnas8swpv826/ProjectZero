6829d1881bd90db582b197779a5f760df449a7b6
Code <mac92j@pm.me>
2021-03-01 19:31:46 -0500

    Build dynamic "Add Transaction" functionality in Transactions component

--- app/javascript/components/Transactions.jsx
+++ app/javascript/components/Transactions.jsx
@@ -7,11 +7,17 @@ class Transactions extends React.Component {
     this.state = {
       transactions: [],
       isLoaded: false,
-      addingTransaction: false
+      addingTransaction: false,
+      payee: "",
+      description: "",
+      amount_out: "",
+      messages: []
     };
     this.loadTransactions = this.loadTransactions.bind(this);
     this.addTransactionClick = this.addTransactionClick.bind(this);
     this.cancelClick = this.cancelClick.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   loadTransactions() {
@@ -41,8 +47,54 @@ class Transactions extends React.Component {
     this.setState({addingTransaction: false});
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+
+    const url = "/api/transactions";
+    const { payee, description, amount_out, messages } = this.state;
+    const body = { payee, description, amount_out };
+    const token = document.querySelector("meta[name='csrf-token']").content;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-TOKEN": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        else {
+          throw response;
+        }
+      })
+      .then(() => {
+        this.loadTransactions();
+        this.setState({payee: "", description: "", amount_out: "", messages: []});
+      })
+      .catch((error) => {
+        error.json().then((body) => {
+          const keys = Object.keys(body.data);
+          if (keys.includes("payee")) {
+            messages.push(`Payee ${body.data.payee}`);
+          }
+          if (keys.includes("amount_out")) {
+            messages.push(`Amount out ${body.data.amount_out}`);
+          }
+          this.setState({messages: messages});
+        });
+      });
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
-    const {transactions, isLoaded, addingTransaction} = this.state; // Same as const transactions = this.state.transactions (destructuring)
+    const {transactions, isLoaded, addingTransaction, messages} = this.state; // Same as const transactions = this.state.transactions (destructuring)
     const transactionsRows = transactions.map((transaction, index) => (
       <tr key={index}>
         <td>{transaction.payee}</td>
@@ -52,31 +104,38 @@ class Transactions extends React.Component {
     ));
     const addTransactionRow = (
       <tr>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td>
-          <button className="btn btn-primary btn-sm mr-1">Save</button>
-          <button className="btn btn-secondary btn-sm" onClick={this.cancelClick}>Cancel</button>
-        </td>
+        <td><input type="text" name="payee" onChange={this.onChange} value={this.state.payee}/></td>
+        <td><input type="text" name="description" onChange={this.onChange} value={this.state.description}/></td>
+        <td><input type="text" name="amount_out" onChange={this.onChange} value={this.state.amount_out}/></td>
       </tr>
     );
+    const addButtonsAndErrors = (
+      <div className="mt-2">
+        <button className="btn btn-primary btn-sm mr-1" onClick={this.onSubmit}>Save</button>
+        <button className="btn btn-secondary btn-sm" onClick={this.cancelClick}>Cancel</button>
+        <span className="text-danger">{messages.join(". ")}</span>
+      </div>
+    )
     const transactionsTable = (
-      <table>
-        <thead>
-          <tr>
-            <th>Payee</th>
-            <th>Description</th>
-            <th>Amount Out</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactionsRows}
-          {addingTransaction &&
-            addTransactionRow
-          }
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Payee</th>
+              <th>Description</th>
+              <th>Amount Out</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactionsRows}
+            {addingTransaction &&
+              addTransactionRow
+            }
+          </tbody>
+        </table>
+        {addingTransaction &&
+          addButtonsAndErrors}
+      </div>
     )
     const noTransaction = (
       <p>No transactions to show.</p>
