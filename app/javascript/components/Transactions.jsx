bddca45bf2711720b0c46a30f047f8eedab914a6
Code <mac92j@pm.me>
2021-03-12 21:02:19 -0500

    Build update UI with in-line editing

--- app/javascript/components/Transactions.jsx
+++ app/javascript/components/Transactions.jsx
@@ -15,7 +15,8 @@ class Transactions extends React.Component {
       transaction_date: "",
       messages: [],
       selectedRows: [],
-      updating: false
+      updating: false,
+      transactionId: 0
     };
     this.loadTransactions = this.loadTransactions.bind(this);
     this.addTransactionClick = this.addTransactionClick.bind(this);
@@ -29,6 +30,7 @@ class Transactions extends React.Component {
     this.onChange = this.onChange.bind(this);
     this.onCheckboxChange = this.onCheckboxChange.bind(this);
     this.update = this.update.bind(this);
+    this.sendData = this.sendData.bind(this);
   }
 
   loadTransactions() {
@@ -64,14 +66,18 @@ class Transactions extends React.Component {
 
   addSave(event) {
     event.preventDefault();
-
     const url = "/api/transactions";
+    const method = "POST";
+    this.sendData(url, method);
+  }
+
+  sendData(url, method) {
     const { payee, description, amount_out, transaction_date, messages } = this.state;
     const body = { payee, description, amount_out, transaction_date };
     const token = document.querySelector("meta[name='csrf-token']").content;
 
     fetch(url, {
-      method: "POST",
+      method: method,
       headers: {
         "X-CSRF-TOKEN": token,
         "Content-Type": "application/json"
@@ -88,7 +94,7 @@ class Transactions extends React.Component {
       })
       .then(() => {
         this.loadTransactions();
-        this.setState({ payee: "", description: "", amount_out: "", transaction_date: "", messages: []});
+        this.setState({ payee: "", description: "", amount_out: "", transaction_date: "", messages: [] });
       })
       .catch((error) => {
         error.json().then((body) => {
@@ -102,13 +108,16 @@ class Transactions extends React.Component {
           if (keys.includes("transaction_date")) {
             messages.push(`Date ${body.data.transaction_date}`);
           }
-          this.setState({messages: messages});
+          this.setState({ messages: messages });
         });
       });
   }
 
   updateSave() {
-    console.log("It works");
+    const transactionId = this.state.transactionId;
+    const url = `/api/transactions/${transactionId}`;
+    const method = "PATCH";
+    this.sendData(url, method);
   }
 
   deleteCancel() {
@@ -176,20 +185,41 @@ class Transactions extends React.Component {
     this.setState({selectedRows: selectedRows});
   }
 
-  update(event) {
-    this.setState({updating: true, deletingTransactions: false, addingTransaction: false});
+  update(event, transaction) {
+    this.setState({
+      updating: true,
+      deletingTransactions: false,
+      addingTransaction: false,
+      payee: transaction.payee,
+      description: transaction.description,
+      amount_out: transaction.amount_out,
+      transaction_date: transaction.transaction_date,
+      transactionId: transaction.id
+    });
   }
 
   render() {
-    const {transactions, isLoaded, addingTransaction, deletingTransactions, updating, messages} = this.state; // Same as const transactions = this.state.transactions (destructuring)
+    const {transactions, isLoaded, addingTransaction, deletingTransactions, updating, messages, transactionId} = this.state; // Same as const transactions = this.state.transactions (destructuring)
     const transactionsRows = transactions.map((transaction, index) => (
       <tr key={index}>
         {deletingTransactions &&
           <td><input type="checkbox" value={transaction.id} onChange={this.onCheckboxChange}/></td>}
-        <td onClick={this.update}>{transaction.transaction_date}</td>
-        <td>{transaction.payee}</td>
-        <td>{transaction.description}</td>
-        <td>{transaction.amount_out}</td>
+        {(updating && transactionId == transaction.id) ?
+          <td><input type="date" name="transaction_date" onChange={this.onChange} value={this.state.transaction_date} /></td> :
+          <td onClick={(event) => this.update(event, transaction)}>{transaction.transaction_date}</td>
+        }
+        {(updating && transactionId == transaction.id)?
+          <td><input type="text" name="payee" onChange={this.onChange} value={this.state.payee}/></td> :
+          <td onClick={(event) => this.update(event, transaction)}>{transaction.payee}</td>
+        }
+        {(updating && transactionId == transaction.id) ?
+          <td><input type="text" name="description" onChange={this.onChange} value={this.state.description} /></td> :
+          <td onClick={(event) => this.update(event, transaction)}>{transaction.description}</td>
+        }
+        {(updating && transactionId == transaction.id) ?
+          <td><input type="text" name="amount_out" onChange={this.onChange} value={this.state.amount_out} /></td> :
+          <td onClick={(event) => this.update(event, transaction)}>{transaction.amount_out}</td>
+        }
       </tr>
     ));
     const addTransactionRow = (
