e363eab381a636ede026a00c49fd3e1b5a6862d7
Code <mac92j@pm.me>
2021-05-08 14:47:10 -0400

    Add styling to transactions table

--- app/javascript/components/Transactions/TransactionsTable.jsx
+++ app/javascript/components/Transactions/TransactionsTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles.css';
 
 const TransactionsTable = (props) => {
   const { transactions } = props;
@@ -18,13 +19,13 @@ const TransactionsTable = (props) => {
 const TableHeader = () => (
   <thead>
     <tr>
-      <th>Date</th>
-      <th>Account</th>
-      <th>Payee</th>
-      <th>Category</th>
-      <th>Subcategory</th>
-      <th>Description</th>
-      <th>Amount Out</th>
+      <th className="date-cell">Date</th>
+      <th className="account-cell">Account</th>
+      <th className="payee-cell">Payee</th>
+      <th className="category-cell">Category</th>
+      <th className="category-cell">Subcategory</th>
+      <th className="description-cell">Description</th>
+      <th className="amount-out-cell">Amount Out</th>
     </tr>
   </thead>
 );
@@ -35,13 +36,13 @@ const TableRows = (props) => {
   return (
     transactions.map((transaction) => (
       <tr key={transaction.id}>
-        <td>{transaction.transaction_date}</td>
-        <td>{transaction.account_name}</td>
-        <td>{transaction.payee}</td>
-        <td>{transaction.category_name}</td>
-        <td>{transaction.subcategory_name}</td>
-        <td>{transaction.description}</td>
-        <td>{transaction.amount_out}</td>
+        <td className="date-cell">{transaction.transaction_date}</td>
+        <td className="account-cell">{transaction.account_name}</td>
+        <td className="payee-cell">{transaction.payee}</td>
+        <td className="category-cell">{transaction.category_name}</td>
+        <td className="category-cell">{transaction.subcategory_name}</td>
+        <td className="description-cell">{transaction.description}</td>
+        <td className="amount-out-cell">{transaction.amount_out}</td>
       </tr>
     ))
   );
