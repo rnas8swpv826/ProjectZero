4ce1a54e8cc94ada2b9dd745bd560479eb5be65f
Code <mac92j@pm.me>
2021-05-08 21:43:08 -0400

    Add UI for deleting transactions from table

--- app/javascript/components/Transactions/Transactions.jsx
+++ app/javascript/components/Transactions/Transactions.jsx
@@ -16,12 +16,40 @@ const Transactions = () => {
   }, []);
   // ------------
 
+  // ---- Delete Transactions ----
+  const [deleting, setDeleting] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const selectRowsHandler = (idOfChecked) => {
+    if (!selectedRows.includes(idOfChecked)) {
+      setSelectedRows((prevState) => [...prevState, idOfChecked]);
+      // Add id associated with checkbox only if it's not already checked
+    } else {
+      setSelectedRows((prevState) => prevState.filter((id) => id !== idOfChecked));
+      // Remove id when the checkbox is selected a second time (i.e. unchecked)
+    }
+  };
+  // ------------
+
   return (
     <div className="container">
       <h1>Transactions</h1>
+      <button
+        type="button"
+        className="btn btn-outline-danger mb-3 mr-2"
+        onClick={() => setDeleting(true)}
+      >Delete Transactions
+      </button>
       {isLoading && <p>Transactions are loading.</p>}
       {transactions.length > 0 && !isLoading
-        ? <TransactionsTable transactions={transactions} /> : <p>No transactions to show.</p>}
+        ? (
+          <TransactionsTable
+            transactions={transactions}
+            deleting={deleting}
+            onCheckboxChange={selectRowsHandler}
+          />
+        )
+        : <p>No transactions to show.</p>}
       {error}
     </div>
   );
--- app/javascript/components/Transactions/TransactionsTable.jsx
+++ app/javascript/components/Transactions/TransactionsTable.jsx
@@ -2,50 +2,53 @@ import React from 'react';
 import '../styles.css';
 
 const TransactionsTable = (props) => {
-  const { transactions } = props;
+  const { transactions, deleting } = props;
+
+  const checkboxHandler = (event) => {
+    props.onCheckboxChange(event.target.value);
+  };
 
   return (
-    <div>
-      <table>
-        <TableHeader />
-        <tbody>
-          <TableRows transactions={transactions} />
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          {deleting && <th> </th>}
+          <TableHeader />
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction) => (
+          <tr key={transaction.id}>
+            {deleting
+              && (
+                <td>
+                  <input type="checkbox" value={transaction.id} onChange={checkboxHandler} />
+                </td>
+              )}
+            <td className="date-cell">{transaction.transaction_date}</td>
+            <td className="account-cell">{transaction.account_name}</td>
+            <td className="payee-cell">{transaction.payee}</td>
+            <td className="category-cell">{transaction.category_name}</td>
+            <td className="category-cell">{transaction.subcategory_name}</td>
+            <td className="description-cell">{transaction.description}</td>
+            <td className="amount-out-cell">{transaction.amount_out}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 const TableHeader = () => (
-  <thead>
-    <tr>
-      <th className="date-cell">Date</th>
-      <th className="account-cell">Account</th>
-      <th className="payee-cell">Payee</th>
-      <th className="category-cell">Category</th>
-      <th className="category-cell">Subcategory</th>
-      <th className="description-cell">Description</th>
-      <th className="amount-out-cell">Amount Out</th>
-    </tr>
-  </thead>
+  <>
+    <th className="date-cell">Date</th>
+    <th className="account-cell">Account</th>
+    <th className="payee-cell">Payee</th>
+    <th className="category-cell">Category</th>
+    <th className="category-cell">Subcategory</th>
+    <th className="description-cell">Description</th>
+    <th className="amount-out-cell">Amount Out</th>
+  </>
 );
-
-const TableRows = (props) => {
-  const { transactions } = props;
-
-  return (
-    transactions.map((transaction) => (
-      <tr key={transaction.id}>
-        <td className="date-cell">{transaction.transaction_date}</td>
-        <td className="account-cell">{transaction.account_name}</td>
-        <td className="payee-cell">{transaction.payee}</td>
-        <td className="category-cell">{transaction.category_name}</td>
-        <td className="category-cell">{transaction.subcategory_name}</td>
-        <td className="description-cell">{transaction.description}</td>
-        <td className="amount-out-cell">{transaction.amount_out}</td>
-      </tr>
-    ))
-  );
-};
 
 export default TransactionsTable;
